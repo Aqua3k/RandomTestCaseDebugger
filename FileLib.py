1df78592be2baef2144b5bcf3fb53a6677148857
dorapon <dorapon2000@gmail.com>
2022-01-17 23:44:24 +0900

    FileLibのdocstringを記述
    to rebase

--- FileLib.py
+++ FileLib.py
@@ -1,3 +1,7 @@
+"""
+テストケースファイルと実行結果ファイルの名前・パスを保存・読み込みするユーティリティ
+"""
+
 import Debug as dl
 import os
 
@@ -5,6 +9,7 @@ inputFileName = ""
 outputFileName = ""
 outFilePath = ""
 def SetFileName(testCaseName, module):
+    """実行するテストケースとその結果ファイルを一時的に記録"""
     global inputFileName, outputFileName, outFilePath
     inputFileName = testCaseName
     outputFileName = module + ".txt"
@@ -12,14 +17,18 @@ def SetFileName(testCaseName, module):
 
     os.makedirs(os.path.join(dl.outPath, outFilePath), exist_ok=True)
 def GetInputFileName():
+    """実行するテストケースファイル名を取得"""
     return inputFileName
 def GetOutputFileName():
+    """実行結果の出力先のファイル名を取得"""
     return outputFileName
 def GetOutputFilePath():
+    """実行結果の出力先パスを取得"""
     return outFilePath
 
 fileContents = []
 def SetFileContents():
+    """テストケースの各行を読み込みfileContentsに保存"""
     global fileContents
     path = GetInputFileName()
     with open(path) as f:
